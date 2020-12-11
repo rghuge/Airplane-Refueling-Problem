9c2114340670c7e910fc0ebc6a7bbb79f90fee3d
rghuge <66924486+rghuge@users.noreply.github.com>
2020-12-10 23:24:15 -0500

    Create generate_instance.py

---
+++ generate_instance.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 23:12:17 2020
+
+"""
+
+"""
+This function takes as input a value n = number of airplanes, and 
+sigma = variance to be used in the normal distribution while generating an instance.
+
+The function returns an instance of the airplane refueling problem as a list of 2-tuples.
+
+"""
+
+def generate_instance(n, sigma):
+    instance = []
+    
+    
+    return instance
+    
