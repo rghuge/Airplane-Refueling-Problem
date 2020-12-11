3c73bd1e3c67276e05d7f0691ceb95760d2c72aa
rghuge <66924486+rghuge@users.noreply.github.com>
2020-12-10 23:33:18 -0500

    Create get_structured_instance.py

---
+++ get_structured_instance.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 23:24:47 2020
+
+"""
+
+"""
+
+This function takes as input an instance of the airplane refueling problem,
+and a value epsilon 
+
+It returns a structured instance of the airplane refueling problem based on epsilon.
+
+"""
+
+def get_structured_instance(instance, epsilon):
+    structured_instance = []
+    
+    
+    
+    return structured_instance
