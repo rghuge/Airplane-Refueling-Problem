f26e13414c2875e1e480d790701eb31ad6cabc2b
rghuge <66924486+rghuge@users.noreply.github.com>
2020-12-10 23:33:51 -0500

    Create get_appx_state_vecs.py

---
+++ get_appx_state_vecs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 23:29:18 2020
+
+
+"""
+
+"""
+This function takes as values n, L, D_hat, epsilon, and returns a set of vectors such that each vector N
+in [n]^L and satisfies \sum_{p=1}^L N(p) * (1 + epsilon)^p <= (1 + epsilon) * D_hat
+
+"""
+
+def get_appx_state_vecs(n, L, epsilon, D_hat):
+    appx_state_vecs = set()
+    
+    
+    return appx_state_vecs
